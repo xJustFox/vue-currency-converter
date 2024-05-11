39f0f6f9febb08f83f6ae1af5d525c914d8083ff
xJustFox <dodydaversa@gmail.com>
2024-05-11 11:56:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script>
+import AppForm from './components/AppForm.vue';
+import { store } from './store.js';
+import axios from "axios";
+
+export default {
+  components: {
+    AppForm,
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<template>
+  <main>
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+      <div class="box p-3 text-light-green">
+
+        <h1 class="text-center text-green py-1">CURRENCY BOOLVERTER</h1>
+
+        <div>
+          <div>*cifra* è uguale a</div>
+          <div class="fs-1">*cifra*</div>
+        </div>
+
+        <AppForm></AppForm>
+
+        <AppForm></AppForm>
+
+        <div class="chart">
+
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+@use './style/partials/variables' as *;
+@use './style/generals.scss' as *;
+
+.container-fluid{
+  height: 100vh;
+  overflow-y: auto;
+}
+
+.box{
+  min-width: 700px;
+  border: 2px solid $my_green;
+  border-radius: 5px;
+}
+
+.chart{
+  width: 100%;
+  height: 300px;
+  background-color: blue;
+}
+</style>
---
+++ src/components/AppForm.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div class="d-flex py-1">
+        <input class="col-6" type="text" name="" id="">
+        <select class="col-6" name="" id="">
+            <option value="">1</option>
+        </select>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
